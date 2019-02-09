6ea6dbea920734e65c6cf842a6b63b5eb61a1e4a
dirisujesse <dirisujesse@gmail.com>
2019-02-09 20:20:50 +0100

    Skelatal code generation, and home page implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { FAQComponent } from './components/faq/faq.component';
+import { HelpComponent } from './components/help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    FAQComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
